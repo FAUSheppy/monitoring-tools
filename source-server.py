f583dab079b5f04ad082af35db6477bafd91c59c
Yannik Schmidt <yannik.schmidt@thermoscan.de>
2020-02-15 17:01:24 +0100

    error handling

--- source-server.py
+++ source-server.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 import valve.source.a2s
+from valve.source import NoResponseError
+import sys
 import argparse
 
 if __name__ == "__main__":
@@ -9,5 +11,13 @@ if __name__ == "__main__":
     parser.add_argument('--port', '-p', type=int, default=27015, help='Target Port')
     args = parser.parse_args()
 
-    with valve.source.a2s.ServerQuerier((args.host, args.port)) as server:
-        print(server.info()["player_count"])
+    try:
+        with valve.source.a2s.ServerQuerier((args.host, args.port)) as server:
+            print("{} players".format(server.info()["player_count"]))
+            sys.exit(0)
+    except NoResponseError:
+        print("No Response from Server")
+    except Exception as e:
+        print("Error: {}".format(e))
+    
+    sys.exit(1)
